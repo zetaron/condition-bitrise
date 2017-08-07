f4c4fca06da32f9836e34a224df5480ce2c04601
Fabian Stegemann <fabian.stegemann@gmx.net>
2017-08-07 17:36:34 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var semver = require('semver')
+var SRError = require('@semantic-release/error')
+
+module.exports = function (pluginConfig, config, cb) {
+  var env = config.env
+  var options = config.options
+
+  if (env.CI !== 'true') {
+    return cb(new SRError(
+      'semantic-release didn’t run on Bitrise and therefore a new version won’t be published.\n' +
+      'You can customize this behavior using "verifyConditions" plugins: git.io/sr-plugins',
+      'ENOCI'
+    ))
+  }
+
+  if (env.hasOwnProperty('BITRISE_PULL_REQUEST') && env.BITRISE_PULL_REQUEST !== 'false') {
+    return cb(new SRError(
+      'This test run was triggered by a pull request and therefore a new version won’t be published.',
+      'EPULLREQUEST'
+    ))
+  }
+
+  if (options.branch === env.BITRISE_GIT_BRANCH) return cb(null)
+
+  if (semver.valid(env.BITRISE_GIT_TAG)) {
+    return cb(new SRError(
+      'This test run was triggered by a git tag that was created by semantic-release itself.\n' +
+      'Everything is okay. For log output of the actual publishing process look at the build that ran before this one.',
+      'EGITTAG'
+    ))
+  }
+
+  return cb(new SRError(
+    'This test run was triggered on the branch ' + env.BITRISE_GIT_BRANCH +
+    ', while semantic-release is configured to only publish from ' +
+    options.branch + '.\n' +
+    'You can customize this behavior using the "branch" option: git.io/sr-options',
+    'EBRANCHMISMATCH'
+  ))
+}
---
+++ test.js
@@ -0,0 +1,86 @@
+var test = require('tap').test
+var SRError = require('@semantic-release/error')
+
+var condition = require('./')
+
+test('raise errors in codeship environment', function (t) {
+  t.test('only runs on ci', function (tt) {
+    tt.plan(2)
+
+    condition({}, {env: {}}, function (err) {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'ENOCODESHIP')
+    })
+  })
+
+  t.test('not running on pull requests', function (tt) {
+    tt.plan(2)
+    condition({}, {
+      env: {
+        CI: 'true',
+        BITRISE_PULL_REQUEST: 'true'
+      }
+    }, function (err) {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EPULLREQUEST')
+    })
+  })
+
+  t.test('only running on specified branch', function (tt) {
+    tt.plan(5)
+
+    condition({}, {
+      env: {
+        CI: 'true',
+        BITRISE_GIT_BRANCH: 'master'
+      },
+      options: {
+        branch: 'master'
+      }
+    }, function (err) {
+      tt.is(err, null)
+    })
+
+    condition({}, {
+      env: {
+        CI: 'true',
+        BITRISE_GIT_BRANCH: 'notmaster'
+      },
+      options: {
+        branch: 'master'
+      }
+    }, function (err) {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EBRANCHMISMATCH')
+    })
+
+    condition({}, {
+      env: {
+        CI: 'true',
+        BITRISE_GIT_BRANCH: 'master'
+      },
+      options: {
+        branch: 'foo'
+      }
+    }, function (err) {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EBRANCHMISMATCH')
+    })
+  })
+
+  t.test('not running on tags', function (tt) {
+    tt.plan(2)
+    condition({}, {
+      env: {
+        CI: 'true',
+        BITRISE_GIT_TAG: 'v1.0.0'
+      },
+      options: {}
+    }, function (err) {
+      tt.ok(err instanceof SRError)
+      tt.is(err.code, 'EGITTAG')
+    })
+  })
+
+  t.end()
+})
